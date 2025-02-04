a8606f4a24dfdc299ff75e2db2121b3943941cd6
Tiago Leite <tiagobscleite@gmail.com>
2025-02-04 19:44:13 +0000

    grandes progresso no exec.c
    até agora dá para executar comandos tanto com ou sem o path, se for dado um comando que não existe é emitida uma mensagem a avisar e está sem leaks de memória

--- exec.c
+++ exec.c
@@ -3,25 +3,110 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: strodrig <strodrig@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: tborges- <tborges-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 14:48:54 by tborges-          #+#    #+#             */
-/*   Updated: 2025/01/30 18:22:33 by strodrig         ###   ########.fr       */
+/*   Updated: 2025/02/04 19:41:56 by tborges-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
-int main(int argc, char **argv)
+/**
+ * Verifica se o comando passado como argumento é um PATH (se tiver uma '/' signifca que o utilizador quer
+ * usar um comando numa determinada localização) ou se é um comando simples (nesse caso atribuimos o PATH
+ * com o getenv e depois usamos o strtok para dividir o comando e os seus argumentos).
+ *
+ * Esta função não executa comandos, apenas verifica se o comando passado como argumento é um PATH ou não e
+ * deixa o comando pronto para ser executado.
+ */
+char	*find_executable(char *cmd)
 {
-	int val = execve("/bin/ls", argv, NULL);
-	if (val == -1)
-	{
-		perror("Error");
-	}
-	return (0);
+	char	*path;
+	char	*path_copy;
+	char	*token;
+	char	full_path[1024];
 
+	if (strchr(cmd, '/'))
+	{
+		if (access(cmd, X_OK) == 0)
+			return strdup(cmd);
+		return (NULL);
+	}
+	path = getenv("PATH");
+	if (!path)
+		return (NULL);
+	path_copy = strdup(path);
+	if (!path_copy)
+		return (NULL);
+	token = strtok(path_copy, ":");
+	while (token)
+	{
+		snprintf(full_path, sizeof(full_path), "%s/%s", token, cmd);
+		if (access(full_path, X_OK) == 0)
+		{
+			free(path_copy);
+			return (strdup(full_path));
+		}
+		token = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return (NULL);
 }
 
-//Os comandos basicos do linux, como cd, pwd e ls são importados direto do char **env da main
+/**
+ * Executa o comando passado como argumento.
+ *
+ * Cria um processo filho com o fork e executa o comando no processo filho.
+ */
+void	execute_command(char *cmd, char **args, char **envp)
+{
+	char	*exec_path;
+	pid_t	pid;
+
+	exec_path = find_executable(cmd);
+	if (!exec_path)
+	{
+		fprintf(stderr, "minishell: command not found: %s\n", cmd);
+		return ;
+	}
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return ;
+	}
+	if (pid == 0)
+	{
+		if (execve(exec_path, args, envp) == -1)
+		{
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+		waitpid(pid, NULL, 0);
+	free(exec_path);
+}
+
+
+
+/**
+ * main só para testar
+ */
+int main(int argc, char **argv, char **envp)
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s <command> [args...]\n", argv[0]);
+        return 1;
+    }
+    execute_command(argv[1], &argv[1], envp);
+    return 0;
+}
--- leite/src/minishell.c
+++ leite/src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: tborges- <tborges-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 17:24:29 by tborges-          #+#    #+#             */
-/*   Updated: 2025/02/03 18:01:27 by tborges-         ###   ########.fr       */
+/*   Updated: 2025/02/04 17:24:58 by tborges-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void	handle_signal(int sig)
-{
-	if (sig == SIGINT)
-	{
-		printf("\n");
-		rl_on_new_line();
-		rl_replace_line("", 0);
-		rl_redisplay();
-	}
-}
+// void	handle_signal(int sig)
+// {
+// 	if (sig == SIGINT)
+// 	{
+// 		printf("\n");
+// 		rl_on_new_line();
+// 		rl_replace_line("", 0);
+// 		rl_redisplay();
+// 	}
+// }
 
-void	setup_signals(void)
-{
-	signal(SIGINT, handle_signal);
-	signal(SIGQUIT, SIG_IGN);
-}
+// void	setup_signals(void)
+// {
+// 	signal(SIGINT, handle_signal);
+// 	signal(SIGQUIT, SIG_IGN);
+// }
 
 void	minishell_loop(void)
 {
 	char	*input;
 
-	setup_signals();
+	// setup_signals();
 	while (1)
 	{
 		input = readline("minishell$ ");
 		if (!input) // Ctrl+D
 		{
 			printf("exit\n");
-			rl_clear_history();
+			clear_history();
 			break ;
 		}
 		if (*input)
